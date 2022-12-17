bfade200d350848a6360f3b978e130609aabf959
RenatoAlexandrini <re.alexandrini@gmail.com>
2022-12-17 02:48:50 -0300

    correcao das tabelas e do migration

--- src/database/migrations/migrations.ts
+++ src/database/migrations/migrations.ts
@@ -1,7 +1,10 @@
-import { BaseDatabase } from '../baseDatabase';
+import { BaseDatabase } from "../baseDatabase"
+import { TABLE_STUDENTS, TABLE_INSTRUCTORS, TABLE_TEAMS, TABLE_HOBBIES, TABLE_SPECIALTIES } from './tableNames';
 import students from './students.json';
 import instructors from './instructors.json';
 import teams from './teams.json';
+import hobbies from './hobbies.json';
+import specialities from './specialties.json';
 
 export abstract class MigrationDataBase extends BaseDatabase {
 
@@ -9,32 +12,44 @@ export abstract class MigrationDataBase extends BaseDatabase {
 
         const createTables = async () => {
             await MigrationDataBase.connection.raw(`
-DROP TABLE IF EXISTS  "LabeSystem_students", "LabeSystem_instructors", "LabeSystem_teams" ;);
+DROP TABLE IF EXISTS ${TABLE_STUDENTS}, ${TABLE_INSTRUCTORS}, ${TABLE_TEAMS}, ${TABLE_HOBBIES}, ${TABLE_SPECIALTIES};
 
-CREATE TABLE IF NOT EXISTS "LabeSystem_teams"(
+CREATE TABLE IF NOT EXISTS ${TABLE_TEAMS}(
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    module VARCHAR(255) DEFAULT "0",
+    module VARCHAR(255) DEFAULT "0"
 );
 
-CREATE TABLE IF NOT EXISTS "LabeSystem_students"(
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    birth DATE NOT NULL,
-    team_id VARCHAR(255),
+CREATE TABLE IF NOT EXISTS ${TABLE_HOBBIES}(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     hobby VARCHAR(255)
-    FOREIGN KEY (team_id) REFERENCES "LabeSystem_teams(id),
 );
 
-CREATE TABLE IF NOT EXISTS "LabeSystem_instructors"(
+CREATE TABLE IF NOT EXISTS ${TABLE_SPECIALTIES}(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    specialty varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS ${TABLE_STUDENTS}(
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     birth DATE NOT NULL,
-    team_id VARCHAR(255),
-    specialty VARCHAR(255)
-    FOREIGN KEY (team_id) REFERENCES "LabeSystem_teams(id),
+    team_id VARCHAR(255) NOT NULL,
+    hobby_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES ${TABLE_TEAMS}(id),
+    FOREIGN KEY (hobby_id) REFERENCES ${TABLE_HOBBIES}(id)
+);
+
+CREATE TABLE IF NOT EXISTS ${TABLE_INSTRUCTORS}(
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    birth DATE NOT NULL,
+    team_id VARCHAR(255) NOT NULL,
+    specialty_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES ${TABLE_TEAMS}(id),
+    FOREIGN KEY (specialty_id) REFERENCES ${TABLE_SPECIALTIES}(id)
 );
     `)
                 .then(() => {
@@ -46,19 +61,29 @@ CREATE TABLE IF NOT EXISTS "LabeSystem_instructors"(
 
         const insertData = async () => {
             try {
-                await MigrationDataBase.connection("LabeSystem_students")
-                    .insert(students)
-                    .then(() => console.log(`LabeSystem_students populated!`))
+                await MigrationDataBase.connection(`${TABLE_HOBBIES}`)
+                    .insert(hobbies)
+                    .then(() => console.log(`${TABLE_HOBBIES} populated!`))
                     .catch((error: any) => printError(error))
 
-                await MigrationDataBase.connection("LabeSystem_instructors")
-                    .insert(instructors)
-                    .then(() => console.log(`LabeSystem_instructors populated!`))
+                await MigrationDataBase.connection(`${TABLE_SPECIALTIES}`)
+                    .insert(specialities)
+                    .then(() => console.log(`${TABLE_SPECIALTIES} populated!`))
                     .catch((error: any) => printError(error))
 
-                await MigrationDataBase.connection("LabeSystem_teams")
+                await MigrationDataBase.connection(`${TABLE_TEAMS}`)
                     .insert(teams)
-                    .then(() => console.log(`LabeSystem_teams populated!`))
+                    .then(() => console.log(`${TABLE_TEAMS} populated!`))
+                    .catch((error: any) => printError(error))
+
+                await MigrationDataBase.connection(`${TABLE_STUDENTS}`)
+                    .insert(students)
+                    .then(() => console.log(`${TABLE_STUDENTS} populated!`))
+                    .catch((error: any) => printError(error))
+
+                await MigrationDataBase.connection(`${TABLE_INSTRUCTORS}`)
+                    .insert(instructors)
+                    .then(() => console.log(`${TABLE_INSTRUCTORS} populated!`))
                     .catch((error: any) => printError(error))
 
             } catch (error: any) {
---
+++ src/database/migrations/tableNames.ts
@@ -0,0 +1,6 @@
+
+export const TABLE_STUDENTS = "LabeSystem_Students"
+export const TABLE_INSTRUCTORS = "LabeSystem_Instructors"
+export const TABLE_TEAMS = "LabeSystem_Teams"
+export const TABLE_HOBBIES = "LabeSystem_Hobbies"
+export const TABLE_SPECIALTIES = "LabeSystem_Specialties"
